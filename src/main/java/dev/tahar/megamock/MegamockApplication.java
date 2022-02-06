1aff57784a4a8ff9d8deb378e4378567e588a742
tntmeijs <tntmeijs@gmail.com>
2022-02-06 14:53:31 +0100

    chore: initial commit

---
+++ src/main/java/dev/tahar/megamock/MegamockApplication.java
@@ -0,0 +1,13 @@
+package dev.tahar.megamock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegamockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegamockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tahar/megamock/MegamockApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tahar.megamock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegamockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
